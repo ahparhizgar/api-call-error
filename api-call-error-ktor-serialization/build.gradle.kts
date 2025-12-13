5d5d5f63e480a00216a8d2d535f289ef33156c05
Amir Hossein Parhizgar <ahparhizgar@gmail.com>
2025-12-13 23:26:41 +0330

    Add base project files

---
+++ api-call-error-ktor-serialization/build.gradle.kts
@@ -0,0 +1,88 @@
+import com.android.build.api.dsl.androidLibrary
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.android.kotlin.multiplatform.library)
+    alias(libs.plugins.kotlinx.serialization)
+//    alias(libs.plugins.vanniktech.mavenPublish)
+}
+
+group = "com.ahparhizgar.apicallerror"
+version = "0.1.0"
+
+kotlin {
+    jvm()
+    androidLibrary {
+        namespace = "com.ahparhizgar.apicallerror"
+        compileSdk = libs.versions.android.compileSdk.get().toInt()
+        minSdk = libs.versions.android.minSdk.get().toInt()
+
+        withJava() // enable java compilation support
+        withHostTestBuilder {}.configure {}
+        withDeviceTestBuilder {
+            sourceSetTreeName = "test"
+        }
+
+        compilations.configureEach {
+            compilerOptions.configure {
+                jvmTarget.set(
+                    JvmTarget.JVM_11
+                )
+            }
+        }
+    }
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+    linuxX64()
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.ktor.client.core)
+            implementation(libs.ktor.client.content.negotiation)
+            implementation(libs.ktor.serialization.kotlinx.json)
+            implementation(libs.kotlinx.serialization.json)
+        }
+
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+            implementation(libs.ktor.client.mock)
+            implementation(libs.coroutines.test)
+        }
+    }
+}
+
+//mavenPublishing {
+//    publishToMavenCentral()
+//
+//    signAllPublications()
+//
+//    coordinates(group.toString(), "library", version.toString())
+//
+//    pom {
+//        name = "My library"
+//        description = "A library."
+//        inceptionYear = "2024"
+//        url = "https://github.com/kotlin/multiplatform-library-template/"
+//        licenses {
+//            license {
+//                name = "XXX"
+//                url = "YYY"
+//                distribution = "ZZZ"
+//            }
+//        }
+//        developers {
+//            developer {
+//                id = "XXX"
+//                name = "YYY"
+//                url = "ZZZ"
+//            }
+//        }
+//        scm {
+//            url = "XXX"
+//            connection = "YYY"
+//            developerConnection = "ZZZ"
+//        }
+//    }
+//}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "api-call-error"
+include(":api-call-error-ktor-serialization")
